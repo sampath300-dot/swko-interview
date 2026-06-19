b79c42150ce4738946c244cd2e26fead8b6fd891
Sammpath <sampath.ai23@sahyadri.edu.in>
2026-06-19 11:56:23 +0530

    Changed model to flash lite

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ Format neatly with numbering.
         with st.spinner("Generating Questions..."):
 
             response = client.models.generate_content(
-                model="gemini-2.5-flash",
+                model="gemini-2.0-flash",
                 contents=prompt
             )
 
